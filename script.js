6e572cab614b4d769392b9d201de857c0370707d
thechriskarp <www.chriskarp@gmail.com>
2024-12-26 22:20:28 -0300

    Calculadora minimalista V.1

---
+++ script.js
@@ -0,0 +1,39 @@
+let displayValue = '0';
+const display = document.getElementById('display');
+
+function updateDisplay() {
+    display.textContent = displayValue;
+}
+
+function appendToDisplay(value) {
+    if (displayValue === '0' && value !== '.') {
+        displayValue = value;
+    } else {
+        displayValue += value;
+    }
+    updateDisplay();
+}
+
+function clearDisplay() {
+    displayValue = '0';
+    updateDisplay();
+}
+
+function deleteLast() {
+    displayValue = displayValue.slice(0, -1);
+    if (displayValue === '') {
+        displayValue = '0';
+    }
+    updateDisplay();
+}
+
+function calculate() {
+    try {
+        displayValue = eval(displayValue).toString();
+        updateDisplay();
+    } catch (error) {
+        displayValue = 'Error';
+        updateDisplay();
+        setTimeout(clearDisplay, 1500);
+    }
+}
